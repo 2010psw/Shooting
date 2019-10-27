198e527eaa33a7a36c9d586f59b18e95e0e5b2f1
2010psw <50092524+2010psw@users.noreply.github.com>
2019-10-27 17:09:15 +0900

    .

---
+++ Shooter/Assets/04.Scripts/JoyStickMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoyStickMovement : MonoBehaviour
+{
+    public static JoyStickMovement Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                var instance = FindObjectOfType<JoyStickMovement>();
+                if (instance ==null)
+                {
+                    var instanceContainer = new GameObject("JoyStickMovement");
+                    instance = instanceContainer.AddComponent<JoyStickMovement>();
+                }
+            }
+            return instance;
+        }
+    }
+    public static JoyStickMovement instance;
+
+    public GameObject smallStick;
+    public GameObject bGStick;
+    Vector3 stickFirstPosition;
+    public Vector3 joyVec;
+    float stickRadius;
+    private void Start()
+    {
+        stickRadius = bGStick.gameObject.GetComponent<RectTransform>().sizeDelta.y / 2;
+    }
+    public void PointDown()
+    {
+        bGStick.transform.position = Input.mousePosition;
+        smallStick.transform.position = Input.mousePosition;
+        stickFirstPosition = Input.mousePosition;
+
+    }
+
+    public void Drag(BaseEventData baseEventData)
+    {
+        PointerEventData pointerEventData = baseEventData as PointerEventData;
+        Vector3 DragPosition = pointerEventData.position;
+        joyVec = (DragPosition - stickFirstPosition).normalized;
+
+        float stickDistance = Vector3.Distance(DragPosition, stickFirstPosition);
+
+        if (stickDistance < stickRadius)
+        {
+            smallStick.transform.position = stickFirstPosition + joyVec * stickDistance;
+        }
+        else
+        {
+            smallStick.transform.position = stickFirstPosition + joyVec * stickRadius;
+        }
+    }
+    public void Drop()
+    {
+        joyVec = Vector3.zero;
+    }
+}
---
+++ Shooter/Assets/04.Scripts/PlayerCtrl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerCtrl : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 0.5f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        MoveControl();
+    }
+    
+    void MoveControl()
+    {
+        float moveX = speed * Time.deltaTime * Input.GetAxis("Horizontal");
+        float moveY = speed * Time.deltaTime * Input.GetAxis("Vertical");
+        transform.Translate(moveX, moveY, 0);
+    }
+
+
+
+}

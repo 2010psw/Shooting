f8613692f7c131d07aaa85223bea293fb81f87c0
2010psw <2010psw1@gmail.com>
2019-10-28 22:45:32 +0900

    유저 이동
    조이스틱을 이용한 이동, 화면밖으로 탈출 불가

--- Shooter/Assets/04.Scripts/JoyStickMovement.cs
+++ Shooter/Assets/04.Scripts/JoyStickMovement.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+
 public class JoyStickMovement : MonoBehaviour
 {
-   public static JoyStickMovement Instance
+    public static JoyStickMovement Instance
     {
         get
         {
-            if(instance == null)
+            if (instance ==null)
             {
                 instance = FindObjectOfType<JoyStickMovement>();
-                if (instance == null)
+                if (Instance == null)
                 {
-                    var instanceContainer = new GameObject("StickMovement");
+                    var instanceContainer = new GameObject("JoyStickMovement");
                     instance = instanceContainer.AddComponent<JoyStickMovement>();
                 }
             }
@@ -27,9 +28,39 @@ public class JoyStickMovement : MonoBehaviour
     Vector3 stickFirstPosition;
     public Vector3 joyVec;
     float stickRadius;
-
-    private void Start()
+    Vector3 joyStickFirstPosition;
+    void Start()
     {
-        
+        stickRadius = bGStick.gameObject.GetComponent<RectTransform>().sizeDelta.y / 2;
+        joyStickFirstPosition = bGStick.transform.position;
+    }
+    public void PointDown()
+    {
+        bGStick.transform.position = Input.mousePosition;
+        smallStick.transform.position = Input.mousePosition;
+        stickFirstPosition = Input.mousePosition;
+    }
+    public void Drag( BaseEventData baseEventData)
+    {
+        PointerEventData pointerEventData = baseEventData as PointerEventData;
+        Vector3 DragPosition = pointerEventData.position;
+        joyVec = (DragPosition - stickFirstPosition).normalized;
+
+        float stickDistance = Vector3.Distance(DragPosition, stickFirstPosition);
+
+        if (stickDistance < stickRadius)
+        {
+            smallStick.transform.position = stickFirstPosition + joyVec * stickDistance;
+        }
+        else
+        {
+            smallStick.transform.position = stickFirstPosition + joyVec * stickRadius;
+        }
+    }
+    public void Drop()
+    {
+        joyVec = Vector3.zero;
+        bGStick.transform.position = joyStickFirstPosition;
+        smallStick.transform.position = joyStickFirstPosition;
     }
 }
--- Shooter/Assets/04.Scripts/PlayerCtrl.cs
+++ Shooter/Assets/04.Scripts/PlayerCtrl.cs
@@ -6,18 +6,40 @@ using UnityEngine.EventSystems;
 public class PlayerCtrl : MonoBehaviour
 {
     
+    Rigidbody2D rb;
+    public float speed = 10.0f;
+    public Transform tr;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        tr = GetComponent<Transform>();
     }
-
     
     void Update()
     {
-       
+        //조이스틱 이동
+        move();
     }
     
-    
+    void move()
+    {
+        
+        //조이스틱 이동
+        rb.velocity = new Vector2(JoyStickMovement.Instance.joyVec.x * speed * Time.deltaTime, JoyStickMovement.Instance.joyVec.y * speed * Time.deltaTime);
+        
+        //화면밖으로 안나가게
+        Vector3 p = Camera.main.WorldToViewportPoint(tr.position);
+        if (p.x < 0.04f) p.x = 0.04f;
+        if (p.x > 0.96f) p.x = 0.96f;
+        if (p.y < 0.03f) p.y = 0.03f;
+        if (p.y > 0.96f) p.y = 0.96f;
+        tr.position = Camera.main.ViewportToWorldPoint(p);
+        
 
+    }
+    void shot()
+    {
 
+    }
 }

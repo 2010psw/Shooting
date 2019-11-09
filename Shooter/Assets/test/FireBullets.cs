001a29740807ad41db3593c446cfee9e177556c6
2010psw <2010psw1@gmail.com>
2019-11-09 22:41:15 +0900

    bullet 패턴 추가
    .

--- Shooter/Assets/test/FireBullets.cs
+++ Shooter/Assets/test/FireBullets.cs
@@ -8,67 +8,40 @@ public class FireBullets : MonoBehaviour
     private int bulletsAmount;
 
     [SerializeField]
-    private float startAngle = 0f, endAngle = 360f, DelayTime = 0.5f;
+    private float startAngle = 0f, endAngle = 360f, DelayTime = 0.5f, sa1, ea1;
+
+    [SerializeField]
+    public int Pattern;
 
     private Vector2 bulletMoveDirection;
 
-    public int Pattern;
+
     private void Start()
     {
+        sa1 = startAngle;
+        ea1 = endAngle;
         pattern(Pattern);
     }
 
 
     private void pattern(int pNum)
     {
-        if (pNum == 1)
-        {
-            InvokeRepeating("Fire", 0f, DelayTime);
-        }
         switch (pNum)
         {
-            case 1:
-                InvokeRepeating("Fire", 0f, DelayTime);
-                break;
-            case 2:
+            case 1: //그냥 원
                 InvokeRepeating("Fire1", 0f, DelayTime);
                 break;
-            case 3:
-                InvokeRepeating("Fire3", 0f, DelayTime);
+            case 2: //그냥 원 + 크로스
+                InvokeRepeating("Fire2", 0f,0.6f    );
                 break;
-            case 4:
+            case 3: //꽃 bulletamount = 꽃잎 개수
+                InvokeRepeating("Fire3", 0f, 0.1f);
+                break;
+            case 4: //회전
                 InvokeRepeating("Fire4", 0f, DelayTime);
                 break;
-            case 5:
-                InvokeRepeating("Fire5", 0f, DelayTime);
-                break;
 
         }
-    }
-
-    private void Fire()
-    {
-        float angleStep = (endAngle - startAngle) / bulletsAmount;
-        float angle = startAngle;
-
-        for (int i = 0; i < bulletsAmount + 1; i++)
-        {
-            float bulDirY = transform.position.y + Mathf.Cos((angle * Mathf.PI) / 180f);
-            float bulDirX = transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180f);
-
-            Vector3 bulMoveVector = new Vector3(bulDirX, bulDirY, 0f);
-            Vector2 bulDir = (bulMoveVector - transform.position).normalized;
-
-            GameObject bul = BulletPool.bulletPoolInstance.GetBullet();
-            bul.transform.position = transform.position;
-            bul.transform.rotation = transform.rotation;
-            bul.SetActive(true);
-            bul.GetComponent<Bullet>().SetMoveDirection(bulDir);
-
-            angle += angleStep;
-        }
-
-
     }
 
     private void Fire1()
@@ -78,8 +51,8 @@ public class FireBullets : MonoBehaviour
 
         for (int i = 0; i < bulletsAmount + 1; i++)
         {
-            float bulDirX = transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180f);
             float bulDirY = transform.position.y + Mathf.Cos((angle * Mathf.PI) / 180f);
+            float bulDirX = transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180f);
 
             Vector3 bulMoveVector = new Vector3(bulDirX, bulDirY, 0f);
             Vector2 bulDir = (bulMoveVector - transform.position).normalized;
@@ -92,16 +65,12 @@ public class FireBullets : MonoBehaviour
 
             angle += angleStep;
         }
-        Invoke("Fire2", DelayTime / 2);
 
     }
-
-
 
     private void Fire2()
     {
         float angleStep = (endAngle - startAngle) / bulletsAmount;
-        float test = angleStep / 2;
         float angle = startAngle;
 
         for (int i = 0; i < bulletsAmount + 1; i++)
@@ -117,28 +86,42 @@ public class FireBullets : MonoBehaviour
             bul.transform.rotation = transform.rotation;
             bul.SetActive(true);
             bul.GetComponent<Bullet>().SetMoveDirection(bulDir);
-            if (i == 0)
-            {
-                angle += test;
-            }
-            else
-            {
 
-                angle += angleStep;
-            }
+            angle += angleStep;
         }
+        Invoke("Fire2_1", 0.3f);
+    }
+    private void Fire2_1()
+    {
+        float angleStep = (endAngle - startAngle) / bulletsAmount;
+        float angle = startAngle + angleStep/2;
 
+        for (int i = 0; i < bulletsAmount + 1; i++)
+        {
+            float bulDirY = transform.position.y + Mathf.Cos((angle * Mathf.PI) / 180f);
+            float bulDirX = transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180f);
 
+            Vector3 bulMoveVector = new Vector3(bulDirX, bulDirY, 0f);
+            Vector2 bulDir = (bulMoveVector - transform.position).normalized;
+
+            GameObject bul = BulletPool.bulletPoolInstance.GetBullet();
+            bul.transform.position = transform.position;
+            bul.transform.rotation = transform.rotation;
+            bul.SetActive(true);
+            bul.GetComponent<Bullet>().SetMoveDirection(bulDir);
+
+            angle += angleStep;
+        }
     }
 
 
-    
     private void Fire3()
     {
-        float angleStep = (endAngle - startAngle) / bulletsAmount;
+        int bulletsAmount = 7;//꽃잎 수, 지역변수로 선언
+        float angleStep = 360 / bulletsAmount;
         float angle = startAngle;
-        startAngle += 20;
-        endAngle += 20;
+        startAngle += 5f;
+        endAngle += 5f;
         for (int i = 0; i < bulletsAmount + 1; i++)
         {
             float bulDirY = transform.position.y + Mathf.Cos((angle * Mathf.PI) / 180f);
@@ -155,32 +138,34 @@ public class FireBullets : MonoBehaviour
 
             angle += angleStep;
         }
+        float as1 = 360 / bulletsAmount;
+        float ag1 = sa1;
+        sa1 -= 5f;
+        ea1 -= 5f;
+        for (int i = 0; i < bulletsAmount + 1; i++)
+        {
+            float bulDirY = transform.position.y + Mathf.Cos((ag1 * Mathf.PI) / 180f);
+            float bulDirX = transform.position.x + Mathf.Sin((ag1 * Mathf.PI) / 180f);
 
+            Vector3 bulMoveVector = new Vector3(bulDirX, bulDirY, 0f);
+            Vector2 bulDir = (bulMoveVector - transform.position).normalized;
+
+            GameObject bul = BulletPool.bulletPoolInstance.GetBullet();
+            bul.transform.position = transform.position;
+            bul.transform.rotation = transform.rotation;
+            bul.SetActive(true);
+            bul.GetComponent<Bullet>().SetMoveDirection(bulDir);
+
+            ag1 += as1;
+        }
     }
+
     private void Fire4()
     {
+       
         float angleStep = (endAngle - startAngle) / bulletsAmount;
         float angle = startAngle;
-        startAngle += 147;
-        endAngle += 147;
-        for (int i = 0; i < bulletsAmount + 1; i++)
-        {
-            float bulDirY = transform.position.y + Mathf.Cos((angle * Mathf.PI) / 180f);
-            float bulDirX = transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180f);
-
-            Vector3 bulMoveVector = new Vector3(bulDirX, bulDirY, 0f);
-            Vector2 bulDir = (bulMoveVector - transform.position).normalized;
-
-            GameObject bul = BulletPool.bulletPoolInstance.GetBullet();
-            bul.transform.position = transform.position;
-            bul.transform.rotation = transform.rotation;
-            bul.SetActive(true);
-            bul.GetComponent<Bullet>().SetMoveDirection(bulDir);
-
-            angle += angleStep;
-        }
-        startAngle += 27;
-        endAngle += 27;
+        
         for (int i = 0; i < bulletsAmount + 1; i++)
         {
             float bulDirY = transform.position.y + Mathf.Cos((angle * Mathf.PI) / 180f);
@@ -198,66 +183,7 @@ public class FireBullets : MonoBehaviour
             angle += angleStep;
         }
 
-    }
-    private void Fire5()
-    {
-        float angleStep = (endAngle - startAngle) / bulletsAmount;
-        float angle = startAngle;
-        startAngle += 44f;
-        endAngle += 44f;
-        for (int i = 0; i < bulletsAmount + 1; i++)
-        {
-            float bulDirY = transform.position.y + Mathf.Cos((angle * Mathf.PI) / 180f);
-            float bulDirX = transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180f);
-
-            Vector3 bulMoveVector = new Vector3(bulDirX, bulDirY, 0f);
-            Vector2 bulDir = (bulMoveVector - transform.position).normalized;
-
-            GameObject bul = BulletPool.bulletPoolInstance.GetBullet();
-            bul.transform.position = transform.position;
-            bul.transform.rotation = transform.rotation;
-            bul.SetActive(true);
-            bul.GetComponent<Bullet>().SetMoveDirection(bulDir);
-
-            angle += angleStep;
-        }
-        startAngle += 44f;
-        endAngle += 44f;
-        for (int i = 0; i < bulletsAmount + 1; i++)
-        {
-            float bulDirY = transform.position.y + Mathf.Cos((angle * Mathf.PI) / 180f);
-            float bulDirX = transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180f);
-
-            Vector3 bulMoveVector = new Vector3(bulDirX, bulDirY, 0f);
-            Vector2 bulDir = (bulMoveVector - transform.position).normalized;
-
-            GameObject bul = BulletPool.bulletPoolInstance.GetBullet();
-            bul.transform.position = transform.position;
-            bul.transform.rotation = transform.rotation;
-            bul.SetActive(true);
-            bul.GetComponent<Bullet>().SetMoveDirection(bulDir);
-
-            angle += angleStep;
-        }
-        startAngle += 44f;
-        endAngle += 44f;
-        for (int i = 0; i < bulletsAmount + 1; i++)
-        {
-            float bulDirY = transform.position.y + Mathf.Cos((angle * Mathf.PI) / 180f);
-            float bulDirX = transform.position.x + Mathf.Sin((angle * Mathf.PI) / 180f);
-
-            Vector3 bulMoveVector = new Vector3(bulDirX, bulDirY, 0f);
-            Vector2 bulDir = (bulMoveVector - transform.position).normalized;
-
-            GameObject bul = BulletPool.bulletPoolInstance.GetBullet();
-            bul.transform.position = transform.position;
-            bul.transform.rotation = transform.rotation;
-            bul.SetActive(true);
-            bul.GetComponent<Bullet>().SetMoveDirection(bulDir);
-
-            angle += angleStep;
-        }
-        startAngle += 44f;
-        endAngle += 44f;
+        startAngle += 17f;
+        endAngle += 17f;
     }
 }

653be20eed4bbd3adbae965f866fbe3f9cc18d4e
sosa741 <49966053+sosa741@users.noreply.github.com>
2019-11-12 23:26:00 +0900

    rabbit(쉴드 객체)추가

---
+++ Shooter/Assets/04.Scripts/test/Round.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Round : MonoBehaviour
+{
+    public float angle;
+    public float distance;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float bulDirY = PlayerCtrl.Instance.transform.position.y + Mathf.Cos((angle * Mathf.PI) * distance / 180f);
+        float bulDirX = PlayerCtrl.Instance.transform.position.x + Mathf.Sin((angle * Mathf.PI) * distance / 180f);
+        Vector3 a = new Vector3(bulDirX, bulDirY, 0);
+        this.transform.position = a;
+        angle += 10;
+    }
+}

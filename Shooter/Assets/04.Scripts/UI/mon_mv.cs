af14d506893d95202e49cadffa211b9cb78bec3f
sosa741 <49966053+sosa741@users.noreply.github.com>
2019-11-14 00:33:49 +0900

    패턴 및 스테이지 추가
    2019.11.14

---
+++ Shooter/Assets/04.Scripts/UI/mon_mv.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mon_mv : MonoBehaviour
+{
+    private float moveSpeed;
+    Vector3 a;
+    public static mon_mv Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<mon_mv>();
+                if (Instance == null)
+                {
+                    var instanceContainer = new GameObject("mon_mv");
+                    instance = instanceContainer.AddComponent<mon_mv>();
+                }
+            }
+            return instance;
+        }
+    }
+    private static mon_mv instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 2f;
+        a = Vector3.right;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 p = Camera.main.WorldToViewportPoint(transform.position);
+        if (p.x < 0.3f) a.x = -a.x;
+        if (p.x > 0.7f) a.x = -a.x;
+
+        transform.Translate(a * Time.deltaTime);
+    }
+}
---
+++ Shooter/Assets/04.Scripts/test/mon_1mv.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mon_1mv : MonoBehaviour
+{
+    public float angle;
+    public float distance;
+    private void Start()
+    {
+     
+    }
+
+    private void Update()
+    {
+        float bulDirY = mon_mv.Instance.transform.position.y + Mathf.Cos((angle * Mathf.PI) * distance / 180f);
+        float bulDirX = mon_mv.Instance.transform.position.x + Mathf.Sin((angle * Mathf.PI) * distance / 180f);
+        Vector3 a = new Vector3(bulDirX, bulDirY, 0);
+        this.transform.position = a;
+        angle += 10;
+    }
+}
